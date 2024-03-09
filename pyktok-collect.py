0667e2656b8ef72deabe7a3ef3ca4ac601318dd8
lg107 <158602730+lg107@users.noreply.github.com>
2024-03-08 20:33:26 -0500

    Add files via upload

---
+++ pyktok-collect.py
@@ -0,0 +1,37 @@
+import json
+import pyktok as pyk
+import sys
+import pandas as pd
+
+#### COLLECT DATA
+# python pyktok-collect.py Sec1Gr1_XXXXX.json results_XXXXX.csv
+
+def collect_metadata(inputFile, outputFile):
+    """Call pyktok with a list of URLs to collect post metadata.
+    """
+
+    try:
+        # with open(inputFile) as fin:
+        #     data = json.load(fin)
+        df = pd.read_json(path_or_buf=inputFile, orient='split')
+    except FileNotFoundError:
+        print(f"File '{inputFile}' couldn't be found.")
+        return 
+    
+    try:
+        # urls = [entry['Link'] for entry in data['data']]
+        urls = df['Link'].to_list()
+        # print(len(urls))
+    except:
+        print("There is something wrong with the data format.")
+        return
+    
+    pyk.specify_browser('firefox')
+    pyk.save_tiktok_multi_urls(urls,  # list of URLs to visit
+                               False, # don't save videos   
+                		       outputFile, # csv file
+                		       5) # max time sleep
+    
+if __name__ == "__main__":
+    _, fin, fout = sys.argv
+    collect_metadata(fin, fout)
